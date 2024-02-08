68a7189d4c0be1cc0c8189f8e3e6f328a6d2c215
yozakura0 <ca01972014@st.kawahara.ac.jp>
2024-02-08 16:47:37 +0900

    エフェクトエンジンのドロー処理を実行するようにした

--- like RiskofRain2/k2EngineLow/graphics/RenderingEngine.cpp
+++ like RiskofRain2/k2EngineLow/graphics/RenderingEngine.cpp
@@ -169,6 +169,9 @@ namespace nsK2EngineLow {
 			obj2D->OnRender2D(rc);
 		}
 
+		//エフェクトを描画
+		EffectEngine::GetInstance()->Draw();
+
 		CopyMainRenderTargetToFrameBuffer(rc);
 
 		//shadowSprite.Update({ FRAME_BUFFER_W / -2.0f,FRAME_BUFFER_H / 2.0f,0.0f }, g_quatIdentity, g_vec3One, { 0.0f,1.0f });

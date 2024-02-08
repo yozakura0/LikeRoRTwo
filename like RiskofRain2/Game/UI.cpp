fb40ec16a8d96098616a17ecef3bd0335ddbc582
yozakura0 <ca01972014@st.kawahara.ac.jp>
2024-02-08 16:46:42 +0900

    所持金や経過時間、被ダメ時の赤画像の表示処理を追加

--- like RiskofRain2/Game/UI.cpp
+++ like RiskofRain2/Game/UI.cpp
@@ -1,9 +1,21 @@
 #include "stdafx.h"
 #include "UI.h"
+#include "Game.h"
+#include "Player.h"
 
 UI::UI()
 {
+	m_game = FindGO<Game>("game");
+	m_player = FindGO<Player>("player");
+
 	m_shotTargetRender.Init("Assets/sprite/UI/shotTarget.DDS", 32.0f, 32.0f);
+	m_timerbackRender.Init("Assets/sprite/UI/timerUI.DDS", 200.0f, 70.0f);
+	m_screenFilter.Init("Assets/sprite/UI/damageScreen.DDS", 1920.0f, 1080.0f);
+
+	m_timerbackRender.SetPosition(m_timerPos);
+	m_timerbackRender.Update();
+	m_shotTargetRender.SetPosition(m_shotTargetPos);
+	m_shotTargetRender.Update();
 }
 
 UI::~UI()
@@ -13,11 +25,61 @@ UI::~UI()
 
 void UI::Update()
 {
-	m_shotTargetRender.SetPosition(m_shotTargetPos);
-	m_shotTargetRender.Update();
+	TimerTextSet();
+	MoneyTextSet();
+	FilterCleanness();
+}
+
+void UI::FilterCleanness()
+{
+	if (m_player->IsReceiveDamage() == false)
+	{
+		m_mulAlpha = 0.0f;
+		return;
+	}
+
+	m_mulAlpha += 0.5f;
+	if (m_mulAlpha > 1.0f)
+	{
+		m_mulAlpha = 1.0f;
+	}
+	m_screenFilter.SetMulColor({ 1.0f,1.0f,1.0f,m_mulAlpha });
+}
+
+void UI::TimerTextSet()
+{
+	if (m_second < 10)
+	{
+		swprintf_s(m_timeText, 256, L"%d : %d%d", m_minute, 0, m_second);
+	}
+	else
+	{
+		swprintf_s(m_timeText, 256, L"%d : %d", m_minute, m_second);
+	}
+	m_timerRender.SetText(m_timeText);
+	m_timerRender.SetPosition({ 800.0f,490.0f });
+	m_timerRender.SetScale(1.0f);
+	m_timerRender.SetColor(Vector4::White);
+}
+
+void UI::MoneyTextSet()
+{
+	swprintf_s(m_moneyText, 256, L"%d", m_player->GetMoney());
+	m_moneyRender.SetText(m_moneyText);
+	m_moneyRender.SetPosition({ -800.0f,490.0f });
+	m_moneyRender.SetScale(1.0f);
+	m_moneyRender.SetColor(Vector4::Yellow);
 }
 
 void UI::Render(RenderContext& rc)
 {
+	if (m_player->IsReceiveDamage())
+	{
+		m_screenFilter.Draw(rc);
+	}
+
 	m_shotTargetRender.Draw(rc);
+	m_timerbackRender.Draw(rc);
+	m_timerRender.Draw(rc);
+	m_moneyRender.Draw(rc);
 }
--- like RiskofRain2/Game/UI.h
+++ like RiskofRain2/Game/UI.h
@@ -1,18 +1,49 @@
 #pragma once
 
+class Game;
+class Player;
+
 class UI : public IGameObject
 {
 public:
 	UI();
 	~UI();
 	void Update();
+	void TimerTextSet();
+	void MoneyTextSet();
+	void FilterCleanness();
 	void Render(RenderContext& rc);
 
+	//照準レンダー
 	SpriteRender m_shotTargetRender;
-	SpriteRender m_timerRender;
+	//タイマーの背景レンダー
+	SpriteRender m_timerbackRender;
+	//ダメージを受けたときの画面フィルター
+	SpriteRender m_screenFilter;
+	//タイマーレンダー
+	FontRender m_timerRender;
+	//所持金レンダー
+	FontRender m_moneyRender;
+
+	//時間を設定する
+	void SetTime(int minute, int second)
+	{
+		m_minute = minute;
+		m_second = second;
+	}
 private:
 	const Vector3 m_shotTargetPos = { 0.0f,0.0f,0.0f };
-	//const Vector3 m_
+	const Vector3 m_timerPos = { 850.0f,480.0f,0.0f };
 
+	float m_mulAlpha = 0.0f;
+
+	wchar_t m_timeText[256];
+	int m_minute;
+	int m_second;
+
+	wchar_t m_moneyText[256];
+
+	Game* m_game;
+	Player* m_player;
 };
 
